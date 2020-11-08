41f5b703049594dcd4cca1b4526f28bef23de255
Baquetron <baquedano95@gmail.com>
2020-11-08 19:28:56 +0100

    First query script

--- queries/Fed_rates_table.py
+++ queries/Fed_rates_table.py
@@ -4,6 +4,8 @@ import pandas
 con = sqlite3.connect("data/db/economic_data.sqlite")
 out = sqlite3.connect("data/db/dashboard_data.sqilte")
 
-df = pandas.read_sql_query("", con)
-print(df)
+df = pandas.read_sql_query(
+    "SELECT DATE(Actual_Date_FFFTULD) AS DATE, Actual_FFFTULD, Actual_FFFTLLD FROM FFFTULD, FFFTLLD WHERE Actual_Date_FFFTULD = Actual_Date_FFFTLLD", con)
+#print(df)
+df.to_sql(name="Fed_rates_table", con=out)
 
